9ad35099f0950da7c86d7e35b170b473372e0c28
Alex Osipenko <alexeyvxa@gmail.com>
2022-11-18 20:41:38 +0300

    add exports from brain-gcd-game.js

---
+++ src/brain-gcd-game.js
@@ -0,0 +1,43 @@
+import getRandomNumber from './helpers/get-random.js';
+
+const getGCDpair = () => {
+  const firstValue = getRandomNumber(1, 100);
+  const secondValue = getRandomNumber(1, 100);
+
+  return `${firstValue} ${secondValue}`;
+};
+
+const getGCD = (x, y) => {
+  if (y === 0) return x;
+  return getGCD(y, x % y);
+};
+
+const getGCDfromQuestion = (value) => {
+  const toArray = value.split(' ');
+  const x = toArray[0];
+  const y = toArray[1];
+  const result = getGCD(x, y);
+
+  return result.toString();
+};
+
+const roundCount = 3;
+
+const getQuestionsAndAnswers = () => {
+  let i = 0;
+  const result = [];
+
+  while (i < roundCount) {
+    const question = getGCDpair();
+    const correctAnswer = getGCDfromQuestion(question);
+    result[i] = [question, correctAnswer];
+    i += 1;
+  }
+  return result;
+};
+
+const questionsAndAnswers = getQuestionsAndAnswers();
+
+const challenge = 'Find the greatest common divisor of given numbers.';
+
+export { questionsAndAnswers, challenge };
