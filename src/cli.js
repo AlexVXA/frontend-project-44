fab8dd7afe52e50246e40a0ed1627b65cbfbb562
Alex Osipenko <alexeyvxa@gmail.com>
2022-11-26 17:28:44 +0300

    fix greetings string

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getUsernameAndSayHello = () => {
-  console.log('Welcome to the brain games!');
+  console.log('Welcome to the Brain Games!');
 
   const userName = readlineSync.question('May I have your name?: ');
 
