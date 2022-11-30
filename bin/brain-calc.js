013135f9ae23ee21416804e129221e1f27bcb7e9
Alex Osipenko <alexeyvxa@gmail.com>
2022-11-30 16:11:31 +0300

    refactoring brain-calc-game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../src/game-engine.js';
-import { questionsAndAnswers, challenge } from '../src/brain-calc-game.js';
+import startGame from '../src/brain-calc-game.js';
 
-startGame(questionsAndAnswers, challenge);
+startGame();
