ac9ef27f3f8d5028a7553fcbfbf3b27f178e0f56
Alex Osipenko <alexeyvxa@gmail.com>
2022-11-30 16:24:40 +0300

    refactoring brain-gcd-game

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../src/game-engine.js';
-import { questionsAndAnswers, challenge } from '../src/brain-gcd-game.js';
+import startGame from '../src/brain-gcd-game.js';
 
-startGame(questionsAndAnswers, challenge);
+startGame();
