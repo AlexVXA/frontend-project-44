4627998033d78ed4204200d6d79294abafece5f5
Alex Osipenko <alexeyvxa@gmail.com>
2022-12-02 13:21:53 +0300

    isEven() returns boolean

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,11 +1,11 @@
 import getRandomNumber from '../helpers/get-random.js';
 import startGame from '../game-engine.js';
 
-const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => number % 2 === 0;
 
 const getQuestionAndAnswer = () => {
   const question = getRandomNumber(1, 100);
-  const correctAnswer = isEven(question);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
