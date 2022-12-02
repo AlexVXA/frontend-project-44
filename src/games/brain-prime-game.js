c79a7efe09d2d22efeb5113511d192dccb55b308
Alex Osipenko <alexeyvxa@gmail.com>
2022-12-02 13:27:19 +0300

    isPrime() returns bollean

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -3,14 +3,14 @@ import starGame from '../game-engine.js';
 
 const isPrime = (num) => {
   for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
-    if (num % i === 0) return 'no';
+    if (num % i === 0) return false;
   }
-  return num > 1 ? 'yes' : 'no';
+  return num > 1;
 };
 
 const getQuestionAndAnswer = () => {
   const question = getRandomNumber(1, 100);
-  const correctAnswer = isPrime(question).toString();
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
