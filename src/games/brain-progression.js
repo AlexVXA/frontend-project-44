a895e5ae2a5d33b42b2cd74632d4a836e1aeb304
Alex Osipenko <alexeyvxa@gmail.com>
2022-12-04 13:43:45 +0300

    rewrite brain-progression game

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,56 +1,25 @@
 import getRandomNumber from '../helpers/get-random.js';
 import startGame from '../index.js';
 
-const getProgression = () => {
-  const progLength = getRandomNumber(5, 9);
-  const progStep = getRandomNumber(2, 5);
-  let currentValue = getRandomNumber(1, 100);
-  const result = [currentValue];
-  let i = 1;
+const progLength = () => getRandomNumber(5, 10); // https://disk.yandex.ru/i/-cwbT1etAjGkug
+const intialNumber = () => getRandomNumber(1, 100);
+const progStep = () => getRandomNumber(2, 5); // progr step are chosen randomly from this range
 
-  while (i <= progLength) {
-    currentValue += progStep;
-    result[i] = currentValue;
-    i += 1;
+const getProgression = (length, step, number) => {
+  const result = [];
+  for (let i = number; result.length < length; i += step) {
+    result.push(i);
   }
   return result;
 };
 
-const getIncompleteProgression = () => {
-  const progression = getProgression();
-  const progLength = progression.length - 1;
-  const hiddenValue = '..';
-  const hiddenValueIndex = getRandomNumber(0, progLength);
-  progression[hiddenValueIndex] = hiddenValue;
-
-  return progression.join(' ');
-};
-
-const getHiddenValue = (progression) => {
-  const arr = progression.split(' ');
-  const hiddenValueIndex = arr.indexOf('..');
-  if (hiddenValueIndex === 0) {
-    const x = Number(arr[1]);
-    const y = Number(arr[2]);
-    const progStep = y - x;
-    return x - progStep;
-  }
-  if (hiddenValueIndex === arr.length - 1) {
-    const x = Number(arr[hiddenValueIndex - 1]);
-    const y = Number(arr[hiddenValueIndex - 2]);
-    const progStep = x - y;
-    return x + progStep;
-  }
-  const x = Number(arr[hiddenValueIndex - 1]);
-  const y = Number(arr[hiddenValueIndex + 1]);
-  const progStep = (y - x) / 2;
-  return y - progStep;
-};
-
 const getQuestionAndAnswer = () => {
-  const question = getIncompleteProgression();
-  const correctAnswer = getHiddenValue(question).toString();
-  return [question, correctAnswer];
+  const progression = getProgression(progLength(), progStep(), intialNumber());
+  const hiddenValueIndex = getRandomNumber(0, progression.length - 1);
+  const answer = progression[hiddenValueIndex].toString();
+  progression[hiddenValueIndex] = '..';
+  const question = progression.join(' ');
+  return [question, answer];
 };
 
 const challenge = 'What number is missing in the progression?';
