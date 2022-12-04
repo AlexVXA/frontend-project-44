7506d83b813415e1773cf9579210f692b9560d82
Alex Osipenko <alexeyvxa@gmail.com>
2022-12-04 14:51:04 +0300

    refactor, exclude magick numbers

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -4,7 +4,7 @@ import startGame from '../index.js';
 const isEven = (number) => number % 2 === 0;
 
 const getQuestionAndAnswer = () => {
-  const question = getRandomNumber(1, 100);
+  const question = getRandomNumber();
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
