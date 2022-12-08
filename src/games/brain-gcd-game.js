b0cff738884cf88cc349393f81d2d0c032928206
Alex Osipenko <alexeyvxa@gmail.com>
2022-12-08 21:21:47 +0300

    refactor brain-gcd-game.js

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -3,23 +3,16 @@ import startGame from '../index.js';
 
 const challenge = 'Find the greatest common divisor of given numbers.';
 
-const getPair = () => `${getRandomNumber()} ${getRandomNumber()}`;
-
 const getGCD = (x, y) => {
   if (y === 0) return x;
   return getGCD(y, x % y);
 };
 
-const getGCDfromQuestion = (value) => {
-  const [x, y] = value.split(' ');
-  const result = getGCD(x, y);
-
-  return result.toString();
-};
-
 const getQuestionAndAnswer = () => {
-  const question = getPair();
-  const correctAnswer = getGCDfromQuestion(question);
+  const firstValue = getRandomNumber();
+  const secondValue = getRandomNumber();
+  const question = `${firstValue} ${secondValue}`;
+  const correctAnswer = getGCD(firstValue, secondValue).toString();
   return [question, correctAnswer];
 };
 
